75bc351d39451f6af33b9d080253724c9b5cd384
Masud Rana <mr007masudrana@gmail.com>
2025-05-07 11:16:41 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,20 @@
+//*******Array******
+
+const numbers =  [2, 5, 12, 14, 7, 47 ];
+console.log(numbers);
+
+// Making different types array
+
+const marks = [87, 74, 75, 84, 95]; //number type array
+const friends = ['abul', 'kabul', 'babul', 'habul'];  // string type array
+const passed = [true, false, true, false];  // bolean type array
+
+//how to check array length (how many items are there?)
+
+const number = [12, 5, 87, 14, 9, 74, 2];
+const size = number.length;
+console.log(size);
+
+// Practice
+
+1.
