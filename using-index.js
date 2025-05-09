e004acc9aa2fde37ec194a4ea42e3ef86bfe263a
Masud Rana <mr007masudrana@gmail.com>
2025-05-09 12:20:15 +0600

    using index in array

---
+++ using-index.js
@@ -0,0 +1,41 @@
+//******Topic- কার Index কত ছোট*****
+// ***Practice***
+
+// 1. তুই তোর সব বন্ধুদের নাম একটা অ্যারেতে রাখলি। এখন তুই বের কর, 3 নাম্বার ইনডেক্সে কে আছে।
+
+const friends = ['kamal', 'jamal','damal','anower','morshed'];
+console.log(friends[3]);
+
+// 2. তোর 7 টা প্রিয় বই একটা অ্যারেতে রাখ। তারপর দেখ, 5 নাম্বার পজিশনের বইয়ের নাম কি।
+
+const favBooks = ['Marhaba JavaScript', 'Programming Shikho', 'Time Management', 'Positive Thinking', "Slef Book", 'Mind Setup'];
+console.log(favBooks[5]);
+
+// 3. তুই একটা অ্যারে বানালি, যেখাানে 1 থেকে 10 পর্যন্ত সংখ্যাগুলো রাখলি। এখন 7 নাম্বার পজিশনের মানকে 30 দিয়ে আপডেট কর।
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+numbers[7] = 30;
+console.log(numbers);
+
+// 4. তোর মোবাইলে কিছু গেম আছে এবং তুই সেগুলো একটা অ্যারেতে রাখলি- ‘ফ্রি ফায়ার’, ‘পাবজি’, ‘ক্যান্ডি ক্রাশ’, ‘টেম্পল রান’। এখন তুই চাস, 2 নাম্বার ইন্ডেক্সের গেমটা পরিবর্তন করে সেখানে ‘সাবওয়ে সার্ফার’ রাখবি। একটা প্রোগ্রাম লিখে সেটাই কর।
+
+const myPhoneGames = ['free fire', 'papgi', 'candy crush', 'tample run',];
+myPhoneGames[2] = 'Subway Surfer';
+
+console.log(myPhoneGames);
+
+// 5. তোর পরিবারের সদস্যের নাম একটা অ্যারেতে রাখ। তারপর থার্ড পজিশনের সদস্যের নাম আউটপুট হিসেবে দেখা।
+
+const familyMembers = ['Shaheda', 'Mehrab', 'sabiha', 'masud', 'amma'];
+console.log(familyMembers[3]);
+
+//6. তুই প্ল্যান করলি 7টা দেশ ঘুরবি। দেশগুলোর নাম একটা অ্যারেতে রাখ। তারপর 4 নাম্বার পজিশনের দেশের নাম বের কর।
+
+const travelCountries = ['USA','Canada', 'Japan', 'South Korea', 'England', 'Newziland', 'Germany'];
+console.log(travelCountries[4]);
+
+//7. তোর পড়ার টেবিলে 4টা জিনিস আছে। সেগুলোর নাম একটা অ্যারেতে রাখ। তারপর 7 নাম্বার পজিশনের জিনিসটা কী, বের কর।
+
+const thingsTable = ['glass', 'laptop', 'pen', 'diary'];
+console.log(thingsTable[7]);
+
